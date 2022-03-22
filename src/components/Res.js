8ac4f11ac9ebc1074442c6262a7f8a8f08e0bbbd
Alex <demilisher1@gmail.com>
2022-03-22 22:12:14 +0600

    начал делать стили, но уже понял что где то ошибся со структурой

--- src/components/Res.js
+++ src/components/Res.js
@@ -5,10 +5,16 @@ class Res extends React.Component {
     render() {
         return (
             <div className={style.container}>
-                <img src={avatar.src} alt='My Photo' className="avatar"/>
-                {/*<div className={style.textinfo}>*/}
-                {/*    Hello*/}
-                {/*</div>*/}
+                <img src={avatar.src} alt='My Photo' className={style.avatar}/>
+                <div className={style.new}>
+                    <div>
+                        <h1>Алексей Шуляков</h1>
+                        <div>Я основатель сервисного центра <strong>IC-Parts</strong></div>
+                        <div>Я люблю микроэлектронику, пешие прогулки, изучать что-либо новое</div>
+                        <div>Мои социальные сети</div>
+                        <a href="https://vk.com/demilisher" target="_blank">Мой VK</a>
+                    </div>
+                </div>
             </div>
         );
     }
