06467000c087c6130a72ae3663ce8aeca8f373fe
Demilisher <demilisher1@gmail.com>
2022-03-22 12:36:49 +0600

    Initial

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Res from "../src/components/Res/Res";
+
+export default function Res() {
+  return (
+      <div>
+        <Res />
+      </div>
+  )
+}
---
+++ src/components/Res/Res.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import style from "./Res.module.css"
+
+class Res extends React.Component {
+    render() {
+        return(
+            <div className={style.container}>
+                Hello Please help me
+            </div>
+        )
+    }
+}
+export default Res;
