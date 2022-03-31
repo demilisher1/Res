f7cb343408195a6e2e1e2d48da1ffc87249c835c
Demilisher <demilisher1@gmail.com>
2022-03-31 19:47:33 +0600

    создание масивов

--- src/components/Home/components/Skills/Skills.js
+++ src/components/Home/components/Skills/Skills.js
@@ -24,56 +24,35 @@ class Skills extends React.Component {
     };
 
     render() {
-        const {head, body} = this.state;
+        const head = this.state.head;
+        const body = this.state.body;
+        const fold = head.map(function (currentValue, index) {
+            return (
+                <div key={index}>{currentValue}</div>
+            )
+        })
+
+        const list = body.map(function (currentValue,index){
+            const name = currentValue.name;
+            const date = currentValue.date;
+            const position = currentValue.position
+            return (
+                <div key={index} className={style.list}>
+                    <div className={style.name}>{name}</div>
+                    <div className={style.position}>{position}</div>
+                    <div className={style.date}>{date}</div>
+                </div>
+            )
+        })
 
         return (
             <div className={style.works}>
                 <div className={style.my}>Опыт работы</div>
-                <div className={style.head}>
-                    {head.map((value, index) => (
-                        <div key={index}>{value}</div>
-                    ))}
+                <div className={style.items}>
+                    {fold}
                 </div>
-
                 <div>
-                    {body.map(({name, position, date}, index) => (
-                        <div className={style.bodyItem} key={index}>
-                            <div>{name}</div>
-                            <div>{position}</div>
-                            <div>{date}</div>
-                        </div>
-                    ))}
-                </div>
-
-                <div className={style.list}>
-                    <div className={style.name}>
-                        <div>ООО ММС</div>
-                        <div>ООО Мастер ПАК</div>
-                        <div>ЗАО Инмарко</div>
-                        <div>ЗАО Манрос-М</div>
-                        <div>ЗАО Экоил</div>
-                        <div>ООО НИКС</div>
-                        <div>Одноименное ИП</div>
-                    </div>
-                    <div className={style.position}>
-                        <div>Мастер по изготовлению кухонных гарнитуров</div>
-                        <div>Оператор экструзионного оборудования</div>
-                        <div>Грузчик в холодном цехе</div>
-                        <div>Грузчик в теплом складе</div>
-                        <div>Оператор реакторного блока каталитического дигидрирования</div>
-                        <div>Инженер ==> далле Начальник СЦ</div>
-                        <div>Директор ==> Инженер микроэлектронщик	2011 - now
-                        </div>
-                    </div>
-                    <div className={style.date}>
-                        <div>2003 - 2004</div>
-                        <div>2004 - 2005</div>
-                        <div>2005 - 2005</div>
-                        <div>2005 - 2006</div>
-                        <div>2007 - 2007</div>
-                        <div>2007 - 2009</div>
-                        <div>2011 - now</div>
-                    </div>
+                    {list}
                 </div>
             </div>
         );
