817c0edf15f9c877d443880ecc9104fd964627d3
Demilisher <demilisher1@gmail.com>
2022-04-06 18:36:04 +0600

    Изучение методов работы с массивами.

--- src/components/Home/components/Skills/Skills.js
+++ src/components/Home/components/Skills/Skills.js
@@ -1,5 +1,6 @@
 import React from "react";
 import style from './Skills.module.css'
+import {element, string} from "prop-types";
 
 class Skills extends React.Component {
 
@@ -48,17 +49,75 @@ class Skills extends React.Component {
     render() {
         const {head, body} = this.state;
 
-        const newArray = body.filter(function (value,index) {
-            return value.date > "2005 - 2006"
-        })
-        console.log(newArray);
+        // const newArray = body.filter(function (value,index) {
+        //     return value.date > "2005 - 2006"
+        // })
+        // console.log(newArray);
 
-        const Sort = body;
-        body.sort(function Array (x, y) {
-            // return x.name.localeCompare(y.name)
-            return x.position.localeCompare(y.position)
-        })
-        console.log(Sort)
+        // const Sort = body;
+        // body.sort(function Array (x, y) {
+        //     // return x.name.localeCompare(y.name)
+        //     return x.position.localeCompare(y.position)
+        // })
+        // console.log(Sort)
+
+        // console.log(body[1])
+
+        // console.log(body[0].name)
+
+        //  console.log(body.length)
+
+        // console.log(body);
+        // let bodyNew = body;
+        // bodyNew.length = 2;
+        // console.log(body);
+
+        // body[7] = {name: 'NewJob', position: 'Разнорабочий', date: '2000-2000' };
+        // console.log(body);
+
+        // body.push({name: 'NewJob', position: 'Разнорабочий', date: '2000-2000' })
+        // console.log(body);
+
+        // body.shift();
+        // console.log(body);
+
+        // body.pop();
+        // console.log(body);
+
+        // body.unshift({name: 'NewJob', position: 'Разнорабочий', date: '2000-2000' });
+        // console.log(body);
+
+        // delete body[0];
+        // console.log(body);
+        // console.log(body[1]);
+        // console.log(body.length);
+
+         // body.splice(0, 1);
+         // console.log(body);
+
+        // let removed = body.splice(0, 1);
+        // console.log(removed);
+
+        // let newBy = body;
+        // newBy.splice(0, 1,  {name: 'NewJob', position: 'Разнорабочий', date: '2000-2000' });
+        // console.log(newBy);
+
+        // let includes = body;
+        // includes.splice(1, 0, {name: 'NewJob', position: 'Разнорабочий', date: '2000-2000' } );
+        // console.log(includes);
+
+        // let includes = body;
+        // includes.splice(-1, 1);
+        // console.log(includes);
+
+        // let arrOne = body.slice();
+        // console.log(body);
+        //== console.log(arrOne);
+
+        // let arrOne = body.concat({name: 'NewJob', position: 'Разнорабочий', date: '2000-2000' });
+        // console.log(arrOne);
+
+        // console.log(body.indexOf(name: 'ООО ММС')); // Не понял как искать в массиве индекс.
 
         return (
             <div className={style.works}>
