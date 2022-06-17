bc5ee3a9a0e15e94008ee94bb7950640c7d00dea
Demilisher <demilisher1@gmail.com>
2022-06-17 19:39:48 +0600

    hell

--- Context/index.js
+++ Context/index.js
@@ -1,81 +1,253 @@
 
-//
-// function abc() {
-//     console.log(' Внутри функции')
-//     console.log(this);
-//     console.log(3+5/2);
-//     this.style.background = 'red';
+class Fruit {
+    constructor(props) {
+        this.name = props.name;
+        this.color = props.color;
+        this.sens = props.sens;
+    }
+
+    getColor(){
+        return this.color;
+    }
+    getSens() {
+        return this.sens;
+    }
+
+    // static kakDela(){
+    //     return 'норм'
+    // }
+}
+
+class Apple extends Fruit {
+    constructor(props) {
+        super(props);
+        this.countCostochka = props.countCostochka
+    }
+};
+
+class Mango extends Fruit {
+    constructor(props) {
+        super(props);
+        this.tropical = props.tropical;
+    }
+};
+
+class Grape extends Fruit {
+    constructor(props) {
+        super(props);
+        this.howPcs = props.howPcs;
+    }
+};
+
+
+// const ewqeqw = Fruit.kakDela();
+// console.log(ewqeqw);
+
+
+const Yabloko = new Apple({name: 'apple', color: 'red', sens: 'sour', countCostochka: 5});
+const Manga = new Mango({name: 'Mango', color: 'yellow', sens: 'tart', tropical: true});
+const Vinograd = new Grape({name: 'Grape', color: 'green', sens: 'sweet', howPcs: 44 });
+
+const List = [
+    Yabloko,
+    Manga,
+    Vinograd
+]
+
+console.log(List);
+
+// alert(matureOrange.name)
+// alert(greenOrange.name)
+// alert(matureOrange.canYeat)
+
+
+// function Fruits(titleElement) {
+// this.title = titleElement;
 // }
- //abc();
+//
+// let fruit = new Fruits('Apple');
+// let fruit2 = new Fruits('Orange')
+// console.log(fruit.title)
 
-// document.querySelector('p').onclick = abc;
-// p.addEventListener()
+// const obj = {
+//     a: 10,
+//     newFunc: function () {
+//         console.log(this);
+//     },
+//     arrowFunc: () => {
+//         console.log(this)
+//     }
+// };
+// const obj2 = {
+//     asd: function () {
+//        console.log(this)
+//     },
+//     qwe:{
+//         asde: function () {
+//             console.log('2', this)
+//         },
+//         zxc: () => {
+//             console.log('3', this)
+//         }
+//     },
+//     hello: {
+//         aaa: function () {
+//             console.log(this)
+//         },
+//         zzz: function () {
+//             console.log(this)
+//         },
+//     },
+//
+//     zxc: function () {
+//         return [
+//             {
+//                 homeWork3: function () {
+//                     console.log('homeWork3', this)
+//                 }
+//             }
+//         ]
+//     }
+// };
+// const obj3 = {
+//     test1: {
+//         qwe: function (objectName) {
+//             console.log(this)
+//             return obj3.test1[objectName]
+//         },
+//         lesha: {
+//             zdorova: 'aga',
+//             aaa: function () {
+//                 console.log(this)
+//                 return this.zdorova
+//             }
+//         },
+//         alex: {
+//             privet: 'oй boy',
+//             aaa: function () {
+//                 console.log(this)
+//                 return this.privet
+//             }
+//         }
+//     }
+//
+// }
+// obj3.test1.qwe()
+// obj3.test1.lesha.aaa()
+// obj3.test1.alex.aaa()
+//
+// const cheto = obj3.test1.qwe('alex');
+// console.log(cheto)
+// Что выведет консоль?
+// obj2.zxc()[0].homeWork3() //Добрались до функции homeWork3
+// console.log(obj2.zxc()[0].homeWork3())
+// console.log(obj2.zxc()[0].homeWork3)
+// console.log(obj2.zxc()[0]["homeWork3"])
+// function Fruits(name, canYeat) {
+//     this.name = name;
+//     this.canYeat = canYeat;
+// };
+//
+// let fruit = new Fruits();
+//
+// function Orange(name) {
+//     this.name = name;
+// };
+//
+// Orange.prototype = fruit;
+//
+// matureOrange = new Orange('Уральские');
+// greenOrange = new Orange('Пермские');
+//
+// matureOrange.canYeat = true
+// greenOrange.canYeat = false
+//
+// console.log(matureOrange, greenOrange)
 
-// console.log('Глобальный', this);
-
-const obj = {
-    a: 10,
-    newFunc: function () {
-        console.log(this);
-    },
-    arrowFunc: () => {
-        console.log(this)
-    }
-};
-
-const obj2 = {
-    asd: function () {
-       console.log(this)
-    },
-    qwe:{
-        asde: function () {
-            console.log('2', this)
-        },
-        zxc: () => {
-            console.log('3', this)
-        }
-    },
-    hello: {
-        aaa: function () {
-            console.log(this)
-        },
-        zzz: function () {
-            console.log(this)
-        },
-    },
-
-    zxc: function () {
-        return [
-            {
-                homeWork3: function () {
-                    console.log('homeWork3', this)
-                }
-            }
-        ]
-    }
-
-};
+// alert(matureOrange.name)
+// alert(greenOrange.name)
+// alert(matureOrange.canYeat)
 
 
-//HomeWork 1
-const homeWork1 = obj2.qwe;
+// function Fruits(titleElement) {
+// this.title = titleElement;
+// }
+//
+// let fruit = new Fruits('Apple');
+// let fruit2 = new Fruits('Orange')
+// console.log(fruit.title)
 
-homeWork1.asde();
-
-//HomeWork2
-
-// const homeWork2 = obj2.hello.aaa.bind(obj2.hello);
-
-// homeWork2();
-
-//HomeWork 3
-
-// вывести console.log('homeWork3', this)
-
-
-
-
-
-
-
-
+// const obj = {
+//     a: 10,
+//     newFunc: function () {
+//         console.log(this);
+//     },
+//     arrowFunc: () => {
+//         console.log(this)
+//     }
+// };
+// const obj2 = {
+//     asd: function () {
+//        console.log(this)
+//     },
+//     qwe:{
+//         asde: function () {
+//             console.log('2', this)
+//         },
+//         zxc: () => {
+//             console.log('3', this)
+//         }
+//     },
+//     hello: {
+//         aaa: function () {
+//             console.log(this)
+//         },
+//         zzz: function () {
+//             console.log(this)
+//         },
+//     },
+//
+//     zxc: function () {
+//         return [
+//             {
+//                 homeWork3: function () {
+//                     console.log('homeWork3', this)
+//                 }
+//             }
+//         ]
+//     }
+// };
+// const obj3 = {
+//     test1: {
+//         qwe: function (objectName) {
+//             console.log(this)
+//             return obj3.test1[objectName]
+//         },
+//         lesha: {
+//             zdorova: 'aga',
+//             aaa: function () {
+//                 console.log(this)
+//                 return this.zdorova
+//             }
+//         },
+//         alex: {
+//             privet: 'oй boy',
+//             aaa: function () {
+//                 console.log(this)
+//                 return this.privet
+//             }
+//         }
+//     }
+//
+// }
+// obj3.test1.qwe()
+// obj3.test1.lesha.aaa()
+// obj3.test1.alex.aaa()
+//
+// const cheto = obj3.test1.qwe('alex');
+// console.log(cheto)
+// Что выведет консоль?
+// obj2.zxc()[0].homeWork3() //Добрались до функции homeWork3
+// console.log(obj2.zxc()[0].homeWork3())
+// console.log(obj2.zxc()[0].homeWork3)
+// console.log(obj2.zxc()[0]["homeWork3"])
