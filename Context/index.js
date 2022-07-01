f8c9b1207dc7a6444df886d78a32f061debe646f
Demilisher <demilisher1@gmail.com>
2022-07-01 19:37:42 +0600

    fff17

--- Context/index.js
+++ Context/index.js
@@ -1,66 +1,99 @@
 
-class Fruit {
-    constructor(props) {
-        this.name = props.name;
-        this.color = props.color;
-        this.sens = props.sens;
-        // if (props.price === undefined) {
-        //     this.price = 0;
-        // } else {
-        //     this.price = props.price
-        // }
-        this.price = props.price || 0
-    }
-
-    getColor() {
-        return this.color;
-    }
-
-    getSens() {
-        return this.sens;
+function sequence(start, step) {
+    let Init1 = start - step;
+    return function () {
+        return Init1 += step
     }
 }
+// var generator = sequence(10, 3);
+// var generator2 = sequence(7, 1);
+//
+// console.log(generator()); // 10
+// console.log(generator()); // 13
+// console.log(generator2()); // 7
+// console.log(generator()); // 16
+// console.log(generator2()); // 8
 
-class Apple extends Fruit {
-    constructor(props) {
-        super(props);
-        this.countCostochka = props.countCostochka
+function take(gen, x) {
+    let newArray = [];
+    for (let index = 0; index < x; index++) {
+        newArray.push(gen())
     }
-};
+    return newArray
+}
 
-class Mango extends Fruit {
-    constructor(props) {
-        super(props);
-        this.tropical = props.tropical;
-    }
-};
+var gen2 = sequence(0, 2);
 
-class Grape extends Fruit {
-    constructor(props) {
-        super(props);
-        this.howPcs = props.howPcs;
-    }
-};
+
+console.log(take(gen2, 5)); // [0, 2, 4, 6, 8 ]
 
 
 
 
-const Yabloko = new Apple({name: 'apple', color: 'red', sens: 'sour', countCostochka: 5, price: 22});
-const Manga = new Mango({name: 'Mango', color: 'yellow', sens: 'tart', tropical: true});
-const Vinograd = new Grape({name: 'Grape', color: 'green', sens: 'sweet', howPcs: 44, price: 88 });
 
-
-const List = [
-    Yabloko,
-     Manga,
-    Vinograd
-];
-
-const Eblan = [];
-for (let value of List) {
-    Eblan.push(value.name)
-};
-console.log(Eblan)
+//
+// class Fruit {
+//     constructor(props) {
+//         this.name = props.name;
+//         this.color = props.color;
+//         this.sens = props.sens;
+//         // if (props.price === undefined) {
+//         //     this.price = 0;
+//         // } else {
+//         //     this.price = props.price
+//         // }
+//         this.price = props.price || 0
+//     }
+//
+//     getColor() {
+//         return this.color;
+//     }
+//
+//     getSens() {
+//         return this.sens;
+//     }
+// }
+//
+// class Apple extends Fruit {
+//     constructor(props) {
+//         super(props);
+//         this.countCostochka = props.countCostochka
+//     }
+// };
+//
+// class Mango extends Fruit {
+//     constructor(props) {
+//         super(props);
+//         this.tropical = props.tropical;
+//     }
+// };
+//
+// class Grape extends Fruit {
+//     constructor(props) {
+//         super(props);
+//         this.howPcs = props.howPcs;
+//     }
+// };
+//
+//
+//
+//
+// const Yabloko = new Apple({name: 'apple', color: 'red', sens: 'sour', countCostochka: 5, price: 22});
+// const Manga = new Mango({name: 'Mango', color: 'yellow', sens: 'tart', tropical: true});
+// const Vinograd = new Grape({name: 'Grape', color: 'green', sens: 'sweet', howPcs: 44, price: 88 });
+//
+//
+// const List = [
+//     Yabloko,
+//      Manga,
+//     Vinograd
+// ];
+//
+// const Eblan = [];
+// for (let value of List) {
+//     Eblan.push(value.name)
+// };
+// console.log(Eblan)
 
 
 // const newMap = List.map((value) => value.name)
