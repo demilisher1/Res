b5bb0898fa3ba2cd87befceec4bb8d8181aa06cc
demilisher1 <demilisher1@gmail.com>
2022-07-25 19:30:57 +0600

    custom map1

--- Context/customFlat.js
+++ Context/customFlat.js
@@ -1,17 +1,48 @@
-function smartSum(array, result = []) {
-    for (let value of array) {
-        if (Array.isArray(value)) {
-            smartSum(value, result)
-        }  else {
-            result.push(value)
+// function smartSum(array, result = []) {
+//     for (let value of array) {
+//         if (Array.isArray(value)) {
+//             smartSum(value, result)
+//         }  else {
+//             result.push(value)
+//         }
+//     }
+//     return result
+// }
+//
+// const result2 =  smartSum([1,2,[[3,4],5],6]);
+//
+// console.log(result2);
+
+
+// function user(name) {
+// alert('Привет ' + name);
+// };
+//
+// function userInputName(callback) {
+//     let userName = prompt('Введите ваше имя ');
+//     callback(userName)
+// };
+//
+// userInputName(user)
+
+const d = {
+    a: function() {
+        console.log('123')
+    },
+    b: () => {
+        console.log('456')
+    },
+    c: () => {
+        console.log(this)
+        d.a();
+        d.b();
+    },
+    cc: function () {
+        return () => {
+            //хочу тут вызвать а и b
         }
     }
-    return result
 }
+d.cc()();
 
-const result2 =  smartSum([1,2,[[3,4],5],6]);
-
-console.log(result2);
-
-// const result2 =  smartSum(1,2,[[3,4],5],6);
-// console.log(result2)
+// console.log(sdfsd)
